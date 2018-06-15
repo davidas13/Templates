56ccbcd4b6747a1b57af6d4086501107969de886
wongselent <david.agung.satrio@gmail.com>
2018-06-15 21:54:50 +0800

    gg

--- Dev/Auto_ObjID.py
+++ Dev/Auto_ObjID.py
@@ -25,18 +25,7 @@ import pymel.core as pm
 sel = pm.ls(sl=1)
 ob_sel = []
 cur_rl = pm.editRenderLayerGlobals(q=1, crl=1)
-
-def attr(obj, val):
-    cur_rl = pm.editRenderLayerGlobals(q=1, crl=1)
-    er = pm.editRenderLayerAdjustment( cur_rl, q=1, lyr=1)
-    if type(er) is list:
-	    for x in er:
-	        pm.editRenderLayerAdjustment(x, r=1)
-    else:
-	    pm.editRenderLayerAdjustment(obj.objectIDEnabled)
-	    pm.editRenderLayerAdjustment(obj.objectID)
-	    pm.setAttr(obj.objectIDEnabled, val)
-	    pm.setAttr(obj.objectID, 2002)
+er = pm.editRenderLayerAdjustment( cur_rl, q=1, lyr=1)
 
 # ----------------------------------------------------------------------
 # Main Script
@@ -46,18 +35,22 @@ print(pm.editRenderLayerAdjustment( query=True, alg=True ))
 if cur_rl != 'defaultRenderLayer':
     for x in sel:
         try:
-            if pm.getAttr(x.objectIDEnabled) is True:
-                attr(x, 1)
-                ob_sel.append(x)
-                print('\t{}: {} -> {}'.format(x, getAttr(x.objectIDEnabled), getAttr(x.objectID)))
+            ob_sel.append(x)
+            if type(er) is list:
+                for x in er:
+                    print(x)
+                    pm.editRenderLayerAdjustment(x, r=1)
             else:
-                attr(x, 1)
-                ob_sel.append(x)
-                print('\t{}: {}'.format(x, getAttr(x.objectIDEnabled)))
+                pm.editRenderLayerAdjustment(x.objectIDEnabled)
+                pm.editRenderLayerAdjustment(x.objectID)
+                pm.setAttr(x.objectIDEnabled, 1)
+                pm.setAttr(x.objectID, int('200' + str(ob_sel.index(x))))
+                print([i for i in ])
+
+                pm.editRenderLayerAdjustment("vraySettings.renderMaskMode")
+                pm.setAttr("vraySettings.renderMaskMode", 3)
+                pm.editRenderLayerAdjustment("vraySettings.renderMaskObjectIDs")
+                pm.setAttr('vraySettings.renderMaskObjectIDs', 2000, type="string")
+
         except Exception as ex:
-            # print(type(ex))
             print('\t{}: {}'.format(x, ex))
-            #raise AttributeError
-
-
-
